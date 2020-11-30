f2c7bf254f92ba932adbe27e8dab90e5ff9a3427
ljx <172530603@qq.com>
2020-11-30 19:20:14 +0800

    添加了一行注释2020/11/30

--- app/src/main/java/com/example/habitformatter/MainActivity.java
+++ app/src/main/java/com/example/habitformatter/MainActivity.java
@@ -17,6 +17,7 @@ public class MainActivity extends AppCompatActivity {
     private EditText psw;
 
     //test
+    //2020/11/30/ljx
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
